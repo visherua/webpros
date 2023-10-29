e58273b2650e6b2e2f3004a5fd3ea44e5c995e42
Viktor Sherstyuk <slimbaron@Viktors-MacBook-Pro.local>
2023-10-29 17:26:14 +0200

    fix: fix svg

--- pages/layout/images/check-black.tsx
+++ pages/layout/images/check-black.tsx
@@ -8,7 +8,8 @@ const CheckBkack = () => {
       version="1.1"
       preserveAspectRatio="xMidYMid meet"
       xmlns="http://www.w3.org/2000/svg"
-      xmlns:xlink="http://www.w3.org/1999/xlink"
+      xmlnsXlink="http://www.w3.org/1999/xlink"
+
     >
       <title>check-circle-solid</title>
       <path
